3979aebf227515ae481abbaade5ed656212614e2
hiren-patel-devops <abfedena@users.noreply.github.com>
2023-09-05 16:29:14 +0530

    Update DockerHubLogin.groovy

--- vars/DockerHubLogin.groovy
+++ vars/DockerHubLogin.groovy
@@ -1,4 +1,4 @@
-def login(String username, String password,String dockerRegistry ) {
+def call(String username, String password,String dockerRegistry ) {
  
     // Log in to Docker Hub
     sh "echo ${password} | docker login -u ${username} --password-stdin ${dockerRegistry}"
