45dcac4153242ae659f9f2812d1f7d87c08af0a5
hiren-patel-devops <abfedena@users.noreply.github.com>
2023-09-06 16:24:25 +0530

    Update Docker.groovy

--- vars/Docker.groovy
+++ vars/Docker.groovy
@@ -1,8 +1,11 @@
 // my-docker-kubernetes-library/vars/Docker.groovy
 
 
-def buildAndPushImage(String dockerfile, String imageName, String imageTag) {
+def buildAndPushImage() {
     // Logic to build and push Docker image
+    def dockerfile = 'Dockerfile'
+    def imageName = 'ab123cb/shred'
+    def imageTag = 'v1.0'
     sh "docker build -t ${imageName}:${imageTag} -f ${dockerfile} ."
     sh "docker push ${imageName}:${imageTag}"
 } 
